6080070b0e9888cea348d00bc89e397d1b8eb582
ToastCoder <vicky.pcbasic@gmail.com>
2021-02-22 11:38:40 +0530

    Added strbool()

--- main.py
+++ main.py
@@ -11,3 +11,19 @@
 # TOPICS: Deep Learning, TensorFlow, Convolutional Neural Networks, Multiclass Classification
 
 #-------------------------------------------------------------------------------------------------------------------------------
+
+# IMPORTING REQUIRED LIBRARIES
+import os
+import argparse
+
+# FUNCTION TO CONVERT STR INPUT TO BOOL
+def strBool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Expected a Boolean Value.')
+
