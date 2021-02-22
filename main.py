1c6f410279f397b420893638c34357b830f217e4
ToastCoder <vicky.pcbasic@gmail.com>
2021-02-22 11:32:07 +0530

    Some changes

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+#-------------------------------------------------------------------------------------------------------------------------------
 
+# AUTOMATIC MARK DIGITIZATION
+
+# GETS THE IMAGE FROM THE USER AND EXTRACTS THE RESPECTIVE ROLL.NO AND THEIR MARK AND UPDATES IT IN A .CSV FILE.
+
+# FILE NAME: MAIN.PY
+
+# DONE BY: VIGNESHWAR RAVICHANDAR
+
+# TOPICS: Deep Learning, TensorFlow, Convolutional Neural Networks, Binary Classification
+
+#-------------------------------------------------------------------------------------------------------------------------------
