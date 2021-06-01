a7dc7e6c4b0f9aedbc2e4d7fb6f46e54060e544d
ToastCoder <vicky.pcbasic@gmail.com>
2021-06-01 11:58:35 +0530

    Some changes

--- src/test.py
+++ src/test.py
@@ -13,7 +13,22 @@
 #-------------------------------------------------------------------------------------------------------------------------------
 
 # IMPORTING REQUIRED LIBRARIES
+import os
 import cv2
+from PIL import Image
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+
+# FUNCTION FOR IMAGE PREPROCESSING
+def image_preprocessing(img):
+    return None
+
+samples_dir = './samples'
+im = []
+
+for i in os.listdir(samples_dir):
+    i = os.path.join(samples_dir,im)
+    im.append(Image.open(i))
+    
+files = os.listdir(samples_dir)
