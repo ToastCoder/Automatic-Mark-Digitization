d7d3ab9524f7182bd71c24692ab99ff213715b89
ToastCoder <vicky.pcbasic@gmail.com>
2021-06-01 09:08:33 +0530

    Added initial code

---
+++ src/test.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------------------------------------------------------
+
+# AUTOMATIC MARK DIGITIZATION
+
+# GETS THE IMAGE FROM THE USER AND EXTRACTS THE RESPECTIVE ROLL.NO AND THEIR MARK AND UPDATES IT IN A .CSV FILE.
+
+# FILE NAME: TEST.PY
+
+# DONE BY: VIGNESHWAR RAVICHANDAR
+
+# TOPICS: Deep Learning, TensorFlow, Convolutional Neural Networks, Multiclass Classification
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+# IMPORTING REQUIRED LIBRARIES
+import cv2
+import numpy as np
+import pandas as pd
+import tensorflow as tf
